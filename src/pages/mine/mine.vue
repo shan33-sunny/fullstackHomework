5f329003babdf1f0d1ee64429ea56ed838ffa2d3
shan33-sunny <3231956274@qq.com>
2024-03-01 13:15:40 +0800

    first

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,6 +1,20 @@
 <template>
 	<view>
-		
+		<view class="u-flex user-box u-p-30">
+			<view class="u-m-r-10">
+				<v-avatar src="/static/user.jpg" size="100">
+					
+				</v-avatar>
+				
+			</view>
+			<view class="u-flex-1">
+				<view class="u-font-18 u-p-b-20">小星星</view>
+			</view>
+			<view class="u-m-1-10 u-p-10">
+				<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+			</view>
+		</view>
+		<view class="u-m-t-20"></view>
 	</view>
 </template>
 
